22a5e5a164fc435506d2d007ebd34cec742e3f15
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 18:11:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,9 +4,9 @@ def sort_array_asc(array)
   sorted = array.sort
   return sorted
 end
-  
+
 def sort_array_desc(array)
   sorted = []
-  sorted = array.sort 
+  sorted = array.sort
   return sorted.reverse
 end

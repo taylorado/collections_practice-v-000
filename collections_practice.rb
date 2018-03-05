f1b000e71e405b47f166d2766b0b5f15927915b2
Taylor Donlan <taylordonlan@gmail.com>
2018-03-05 18:46:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,7 +54,8 @@ end
 
 def add_s(array)
   array.each_with_index.collect{|string,index|
-    string += "s" if index !=1
-    string = string if index ==1
+    if index !=1
+      string += "s"
+    end
       }
 end
